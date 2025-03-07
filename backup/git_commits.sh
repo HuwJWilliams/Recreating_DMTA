9f4bee7fe4cd37616ab7cc69d0cd4c7278b060c4
Huw Williams <huw.williams.2018@uni.strath.ac.uk>
2025-03-07 21:17:13 +0000

    Automatic commit on 2025-03-07 21:17

--- backup/git_commits.sh
+++ backup/git_commits.sh
@@ -3,9 +3,17 @@
 cd /users/yhb18174/Recreating_DMTA/
 
 git checkout stage
-
 git add .
+git commit -m "Automatic commit on $(date +"%Y-%m-%d %H:%M")"
 
-git commit -m "Automatic commit on $(date +\%Y-\%m-\%d \%H:\%M)"
+# Attempt to push, and capture the exit status.
+if git push -f origin stage; then
+    subject="Automated git backup successful"
+    body="Recreating DMTA backup successful"
+else
+    subject="Automated git backup FAILED"
+    body="Recreating DMTA backup failed. Check logs for details."
+fi
 
-git push -f origin stage
+recipient="huw.williams.2018@uni.strath.ac.uk"
+echo "$body" | mail -s "$subject" "$recipient"
